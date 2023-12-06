61d03aebec4526f7f8f4352ad5d059b1e5aa4e4c
AliReza <ali.reza.buet@gmail.com>
2023-12-06 11:06:50 +0600

    Add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'homepage', to: 'homepage#index'
   Rails.application.routes.draw do
     namespace :api do
       get 'random_greeting', to: 'greetings#random_greeting'
     end
   end
+
+  root 'homepage#index'
 end
