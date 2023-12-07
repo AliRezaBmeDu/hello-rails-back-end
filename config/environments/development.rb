f4356a3958e7b777ebe7dda8a3b9fa3e0ecc0f7e
AliReza <ali.reza.buet@gmail.com>
2023-12-07 14:47:29 +0600

    Update the routes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.enable_reloading = true
 
+  # port
+  # config.port = 4000
+
   # Do not eager load code on boot.
   config.eager_load = false
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
     end
   end
 
-  root 'homepage#index'
+  # root 'homepage#index'
 end
